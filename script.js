44f304903f73339ff6fb11789d9c2c2c6cf07719
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-05 11:05:48 +0300

    answers

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+function getFirstNames(arr) {
+    return arr.map(subArr => subArr[0]);
+  }
+  
+  // Test case
+  console.log(getFirstNames([["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]])); 
+  // ➞ ["Ahmed", "Jaamac", "Mohamed"]
+  
